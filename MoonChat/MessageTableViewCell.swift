4e48928956b36c0e85628ad3ba891f8e1231f58e
Nagase Takuya <JP27916@w022581903a56m.local>
2020-08-26 17:39:59 +0900

    done normal level

--- MoonChat/MessageTableViewCell.swift
+++ MoonChat/MessageTableViewCell.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class MessageTableViewCell: UITableViewCell {
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
--- MoonChat/ViewController.swift
+++ MoonChat/ViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
+import SocketIO
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate {
 
     @IBOutlet weak var messageTableView: UITableView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sendButton: UIButton!
+    private var message: String = ""
+    private var messages: [String]  = []
+    
+    private let socketManager = SocketManager(socketURL: URL(string:"http://10.233.164.162:5000")!, config: [.log(true), .compress])
+    private var socketIOClient : SocketIOClient!
+    
     @IBAction func tapSendButton(_ sender: UIButton) {
+        let m = textField.text ?? ""
+        //message = "{ \"message\": " + m + "}"
+        self.socketIOClient.emit("text", m)
+        //messages.insert(textField.text ?? "", at: 0)
         textField.text = ""
+        //messageTableView.reloadData()
     }
     
     override func viewDidLoad() {
@@ -25,34 +37,106 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         textField.delegate = self
         
         messageTableView.rowHeight = 40
+        messageTableView.tableFooterView = UIView(frame: .zero) //不要なSeparatorが消える
+
         
         // TableViewで利用するオリジナルのTableViewCellを利用するための設定
         let nib = UINib(nibName: "MessageTableViewCell", bundle: nil)
         messageTableView.register(nib, forCellReuseIdentifier: "MessageTableViewCell")
 
+        setupSocketIO()
+        }
         
+    private func setupSocketIO() {
+        //guard let friend = friend else { return }
 
+        socketIOClient = socketManager.defaultSocket
+        
+        socketIOClient.on(clientEvent: .connect){ data, ack in
+            print("socket connected!")
+            self.socketIOClient.emit("joined", "")
+            print("socket joined")
+        }
+        
+        socketIOClient.on(clientEvent: .disconnect){data, ack in
+            print("socket disconnected!")
+        }
+        
+        socketIOClient.on("message") { (data, ack) in
+            print("success")
+            print(data)
+            
+//            if let paydata = data[0] as? NSMutableDictionary {
+//                paydata.
+//            }
+            if let messageArr = data as? [[String:String]] {
+                print("message: \(messageArr)")
+                
+                print(messageArr[0]["msg"]!)
+
+                self.messages.insert(messageArr[0]["msg"]!, at: 0)
+                self.messageTableView.reloadData()
+            }
+        }
+        
+        socketIOClient.connect()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureObserver()  //Notification発行
+    }
+
+
+    // MARK: - Notification
+
+    /// Notification発行
+    func configureObserver() {
+        let notification = NotificationCenter.default
+        notification.addObserver(self, selector: #selector(keyboardWillShow(_:)),
+                                 name: UIResponder.keyboardWillShowNotification, object: nil)
+        notification.addObserver(self, selector: #selector(keyboardWillHide(_:)),
+                                 name: UIResponder.keyboardWillHideNotification, object: nil)
+        print("Notificationを発行")
+    }
+    
+    /// キーボードが表示時に画面をずらす。
+    @objc func keyboardWillShow(_ notification: Notification?) {
+        guard let rect = (notification?.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue,
+            let duration = notification?.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval else { return }
+        UIView.animate(withDuration: duration) {
+            let transform = CGAffineTransform(translationX: 0, y: -(rect.size.height))
+            self.view.transform = transform
+        }
+        print("keyboardWillShowを実行")
+    }
+    
+    /// キーボードが降りたら画面を戻す
+    @objc func keyboardWillHide(_ notification: Notification?) {
+        guard let duration = notification?.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? TimeInterval else { return }
+        UIView.animate(withDuration: duration) {
+            self.view.transform = CGAffineTransform.identity
+        }
+        print("keyboardWillHideを実行")
+    }
+
+}
+
+extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4 // 表示するセルの個数
+        return messages.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // 作成したオリジナルのTableViewCellを利用
         let cell = tableView.dequeueReusableCell(withIdentifier: "MessageTableViewCell", for: indexPath) as! MessageTableViewCell
-        //let cellnumber = String(indexPath.row+1)
-        //cell.label.text = "cell" + cellnumber
-
+        cell.messageLabel.text = messages[indexPath.row]
         return cell
     }
-
 }
 
 extension ViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
-        //message = textField.text ?? ""
         return true
     }
 }
